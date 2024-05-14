e910a4d85bd49aea185c75180625dbf6107f5f6d
PradeepBS <bspradeep50@gmail.com>
2024-05-14 21:51:19 +0530

    Pradeep - handled multiple use cases along with the extended assessment cases and covered the specs for the same.

--- services/string/sum_calculator.rb
+++ services/string/sum_calculator.rb
@@ -4,8 +4,36 @@ class SumCalculator
   def self.add(str)
     return 0 if str.empty?
 
-    delimiters = /[,\n]/
-    numbers = str.split(delimiters).map {|number| number.to_i}
-    numbers.sum
+    # Extract custom delimiter(s) if present
+    delimiter = ',|\n'
+    if str.start_with?('//')
+      custom_delimiters = str[2..-1].split("\n").first
+      # Escape each custom delimiter and combine them
+      escaped_delimiters = custom_delimiters.split('').map { |d| Regexp.escape(d) }.join('|')
+      delimiter = "#{escaped_delimiters}|,|\n"
+      str = str.split("\n").last
+    end
+
+    # Split string by delimiters
+    numbers = str.split(Regexp.new(delimiter))
+    negative_numbers = []
+    sum = 0
+    # loop through the numbers only once to convert to integers and consider numbers > 0 and less than 1001
+    numbers.each do |number|
+      number = number.to_i
+      if number <= 1000
+        if number < 0
+          negative_numbers << number
+        else
+          sum += number
+        end
+      end
+    end
+
+    # Check for negative numbers and raise exception if any
+    if negative_numbers.any?
+      raise ArgumentError, "negatives not allowed: #{negative_numbers.join(', ')}"
+    end
+    sum
   end
 end
--- spec/sum_calculator_spec.rb
+++ spec/sum_calculator_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require_relative '..//services/string/sum_calculator'
+require 'rspec'
 
 RSpec.describe SumCalculator do
   describe "add" do
@@ -12,11 +13,35 @@ RSpec.describe SumCalculator do
         expect(SumCalculator.add("1,2")).to eq(3)
       end
 
-      it "should return the sum if new lines between numbers (instead of commas)" do
+      it "should return the sum if there are new lines between numbers (instead of commas) in the input string" do
         expect(SumCalculator.add("1\n2,3")).to eq(6)
+      end
+
+      it "should support to change delimiter by passing // followed by the new additional delimiter and return the respective sum" do
+        expect(SumCalculator.add("//;\n1;2")).to eq(3)
+      end
+
+      it "should support multiple delimiters" do
+        expect(SumCalculator.add("//[*][%]\n1*2%3")).to eq(6)
+      end
+
+      it "should ignore numbers greater than 1000" do
+        expect(SumCalculator.add("2,1001")).to eq(2)
+      end
+
+      it "should handle delimiters of any length" do
+        expect(SumCalculator.add("//[***]\n1***2***3")).to eq(6)
       end
     end
 
-  end
+    context "invalid scenarios" do
+      it "should throw an exception if negative numbers are passed" do
+        expect { SumCalculator.add('-3,-2') }.to raise_error(ArgumentError, 'negatives not allowed: -3, -2')
+      end
 
+      it "should throw an exception if a single negative number is passed" do
+        expect { SumCalculator.add('-3,2') }.to raise_error(ArgumentError, 'negatives not allowed: -3')
+      end
+    end
+  end
 end
